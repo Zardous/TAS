0c9bfa6e0c95707bc07ec38446470f4f63b4e18f
moserjoki <moserjoki2003@gmail.com>
2026-03-12 09:44:09 +0100

    Added pointcloud tests

--- pointcloud_test.py
+++ pointcloud_test.py
@@ -1,1 +1,19 @@
+import numpy as np
+from pointcloud import PointCloud
+from point import point
+import matplotlib.pyplot as plt
+
+pointcloud_testdata = PointCloud()
+
+for j in range(len(pointcloud_testdata.points)):
+    mean_velocity = np.zeros([len(pointcloud_testdata.points[j])])
+    radial_pos = np.zeros([len(pointcloud_testdata.points[j])])
+    for i, point in enumerate(pointcloud_testdata.points[j]):
+        if point.velocity_mean is not np.nan:
+            mean_velocity[i] = point.velocity_mean
+            radial_pos[i] = point.radial
+
+    #plt.plot(radial_pos, mean_velocity)
+    plt.scatter(radial_pos, mean_velocity)
+plt.legend("0 0.5 1 2 4 7 8")
 
