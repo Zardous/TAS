46179aefa4648aed55ee89bc0c844133c44b0f88
ghulises <g.h.ulises05@gmail.com>
2026-03-17 16:20:34 +0100

    Plotting Halfwidth and midpoint
    pointcloud edited to find midpoint and jet halfwidth, the _test to try and plot these

--- pointcloud_test.py
+++ pointcloud_test.py
@@ -5,7 +5,29 @@ import matplotlib.pyplot as plt
 
 pointcloud_testdata = PointCloud()
 pointcloud_testdata.read_test_data()
-pointcloud_testdata.shift_velocities()
+#pointcloud_testdata.shift_velocities()
+for i in range(len(pointcloud_testdata.points)):
+    lst = pointcloud_testdata.points[i]
+    vel = np.array([p.velocity_mean for p in lst])
+    pos = np.array([p.radial for p in lst])  
+
+    print(f'Axial line {i}:')
+    halfwidth_index, right_up, left_up, right_down, left_down = pointcloud_testdata.find_halfwidth(vel, pos)
+    print(halfwidth_index)
+    print(f'Left down: {pos[left_down]}, Left up: {pos[left_up]}, Right up: {pos[right_up]}, Right down: {pos[right_down]}')
+    print(f'Left down: {left_down}, Left up: {left_up}, Right up: {right_up}, Right down: {right_down}')
+    right_up_max, left_up_max, max, midpoint = pointcloud_testdata.find_mid(vel, pos)
+    print(midpoint)
+    print(f'Left up pos max: {pos[left_up_max]}, Right up pos max: {pos[right_up_max]}, Midpoint: {midpoint}, Max: {max}')
+    print(f'Left up max: {left_up_max}, Right up max: {right_up_max}, Midpoint: {midpoint}, Max: {max}')
+    iteration_num = 0
+    
+    fig, ax = plt.subplots(2, 2)
+
+    plt.scatter(pos, midpoint)
+    plt.scatter(iteration_num, max)
+    plt.scatter(pos, vel)
+    iteration_num += 1
 
 for j in range(len(pointcloud_testdata.points)):
     mean_velocity = np.zeros([len(pointcloud_testdata.points[j])])
