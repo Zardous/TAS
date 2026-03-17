d0044cb121d5be05c953ddc224780691a2b1f2b7
ghulises <g.h.ulises05@gmail.com>
2026-03-17 17:26:43 +0100

    removed useless stuff from pointcloud_test

--- pointcloud_test.py
+++ pointcloud_test.py
@@ -11,13 +11,6 @@ for i in range(len(pointcloud_testdata.points)):
     lst = pointcloud_testdata.points[i]
     vel = np.array([p.velocity_mean for p in lst])
     pos = np.array([p.radial for p in lst])  
-    empty_LD = np.zeros(len(pointcloud_testdata.points[i]))
-    empty_LU = np.zeros(len(pointcloud_testdata.points[i]))
-    empty_RD = np.zeros(len(pointcloud_testdata.points[i]))
-    empty_RU = np.zeros(len(pointcloud_testdata.points[i]))
-    empty_LUM = np.zeros(len(pointcloud_testdata.points[i]))
-    empty_RUM = np.zeros(len(pointcloud_testdata.points[i]))
-    empty_mid = np.zeros(len(pointcloud_testdata.points[i]))
 
     print(f'Axial distance {i}:')
     halfwidth_index, right_up, left_up, right_down, left_down = pointcloud_testdata.find_halfwidth(vel, pos)
@@ -27,13 +20,7 @@ for i in range(len(pointcloud_testdata.points)):
     print(f'Left down: {left_down}, Left up: {left_up}, Right up: {right_up}, Right down: {right_down}')
     right_up_max, left_up_max, max, midpoint = pointcloud_testdata.find_mid(vel, pos)
     print(midpoint)
-    empty_mid[:] = midpoint
-    empty_LD[:] = pos[left_down]
-    empty_LU[:] = pos[left_up]
-    empty_RD[:] = pos[right_down]
-    empty_RU[:] = pos[right_up]
-    empty_LUM[:] = pos[left_up_max]
-    empty_RUM[:] = pos[right_up_max]
+
     print(f'Left up pos max: {pos[left_up_max]}, Right up pos max: {pos[right_up_max]}, Midpoint: {midpoint}, Max: {max}')
     print(f'Left up max: {left_up_max}, Right up max: {right_up_max}, Midpoint: {midpoint}, Max: {max}')
     
