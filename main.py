c434670bee2dd3b4693795de440c8091136c66f6
ethanbullock2486-wq <ethan.bullock2486@gmail.com>
2026-06-09 15:24:24 +0200

    buh

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ cloud.plot_2Dgraph_from_attr_name('velocity_kurtosis', None, ax4)
 #ax2.set_ylim(0)
 #fig.tight_layout()
 
-#fig, ((ax1,ax2)) = plt.subplots(1, 2)
-#cloud.plot_2Dgraph_from_attr_name('velocity_mean', None, ax1)
-#cloud.plot_2Dgraph_from_attr_name('velocity_norm', None, ax2)
+fig, ((ax1,ax2)) = plt.subplots(1, 2)
+cloud.plot_2Dgraph_from_attr_name('velocity_mean', None, ax1)
+cloud.plot_2Dgraph_from_attr_name('velocity_norm', None, ax2)
 
 
-fig, (ax1) = plt.subplots(1, 1)
-cloud.plot_3Dsurface_from_attr_name('velocity_mean', ax1)
+#fig, (ax1) = plt.subplots(1, 1)
+#cloud.plot_3Dsurface_from_attr_name('velocity_mean', ax1)
 
 
 #cloud.plot_2D('velocity_mean', [0], ax4, True)
